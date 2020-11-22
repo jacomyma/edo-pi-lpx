476ca5004c98bb1f8712b6a385b0847f243ce570
jacomyma <mathieu.jacomy@gmail.com>
2020-11-22 18:02:25 +0100

    advancing screen system

--- mido/lpxPads.py
+++ mido/lpxPads.py
@@ -1,4 +1,4 @@
-import mido;
+import mido, math;
 
 # Build pads
 pads_midinote = []
@@ -18,6 +18,47 @@ for note in pads_midinote:
 
 
 # Display
+
+# Display color
+def display(lpx, xy, rgb):
+    pad = xy_to_pad_note(xy)
+    pad_hex = hex(pad)[2:]
+    if len(pad_hex) == 1:
+            pad_hex = '0'+pad_hex
+    r = hex(math.floor(127*rgb[0]))[2:]
+    if len(r) == 1:
+        r = '0'+r
+    g = hex(math.floor(127*rgb[1]))[2:]
+    if len(g) == 1:
+        g = '0'+g
+    b = hex(math.floor(127*rgb[2]))[2:]
+    if len(b) == 1:
+        b = '0'+b
+    hexmsg = 'F0 00 20 29 02 0C 03 03 '+pad_hex+' '+r+' '+g+' '+b+' F7'
+    lpx.send(mido.Message.from_hex(hexmsg))
+
+# Display colors in a series
+def display_multi(lpx, data):
+    hexmsg = 'F0 00 20 29 02 0C 03 '
+    for d in data:
+        xy = d[0]
+        rgb = d[1]
+        pad = xy_to_pad_note(xy)
+        pad_hex = hex(pad)[2:]
+        if len(pad_hex) == 1:
+            pad_hex = '0'+pad_hex
+        r = hex(math.floor(127*rgb[0]))[2:]
+        if len(r) == 1:
+            r = '0'+r
+        g = hex(math.floor(127*rgb[1]))[2:]
+        if len(g) == 1:
+            g = '0'+g
+        b = hex(math.floor(127*rgb[2]))[2:]
+        if len(b) == 1:
+            b = '0'+b
+        hexmsg = hexmsg+'03 '+pad_hex+' '+r+' '+g+' '+b+' '
+    hexmsg = hexmsg+'F7'
+    lpx.send(mido.Message.from_hex(hexmsg))
 
 # Display by velocity code
 def display_vel(lpx, xy, vel):
@@ -74,3 +115,21 @@ def checkMenuMessage(msg):
         return "notes"
     else:
         return False
+
+
+# Display text
+def display_text(lpx, text, loop):
+    if loop:
+        loop_hex = '01'
+    else:
+        loop_hex = '00'
+
+    speed = '10'
+
+    text_bin = text.encode(encoding='utf_8')
+    text_hex = text_bin.hex()
+    
+    hexmsg = 'F0 00 20 29 02 0C 07 '+loop_hex+' '+speed+' 00 03 '+text_hex+' F7'
+    lpx.send(mido.Message.from_hex(hexmsg))
+
+
--- mido/screens.py
+++ mido/screens.py
@@ -1,21 +1,95 @@
 import mido, lpxPads as pads;
 
 def setScreen(settings, lpx, outports, screen):
+    global submenu
     # Reset
     pads.display_reset(lpx, False)
 
-    # Display menu glow
+    # Display menu glow + set submenu
     if screen == 'notes':
         pads.display_menu_glow(lpx, pads.menu['notes']['xy'])
+        submenu = 'edo'
     elif screen == 'settings':
         pads.display_menu_glow(lpx, pads.menu['settings']['xy'])
-        
-    # Test
-    pads.display_vel(lpx, [4,4], 56)
+        submenu = 'settings-1'
 
+    initSubmenu(lpx)
+    
     # Listen to notes
     for msg in lpx:
         if msg.type == "control_change":
             if msg.value == 0:
                 if msg.control == pads.menu[screen]['note']:
                     return 'edo'
+
+def initSubmenu(lpx):
+    if submenu == 'edo':
+        #pads.display(lpx, [2,7], [1,0,0])
+        rgb_low = [0.2,0.2,0.2]
+        rgb_12  = [0.20,0.05,0.30]
+        pads.display_multi(lpx, [
+            [[2,7], rgb_low],   # EDO 03
+            [[3,7], rgb_low],   # EDO 04
+            [[4,7], rgb_low],   # EDO 05
+            [[5,7], rgb_low],   # EDO 06
+            [[6,7], rgb_low],   # EDO 07
+            [[7,7], rgb_low],   # EDO 08
+            [[0,6], rgb_low],   # EDO 09
+            [[1,6], rgb_low],   # EDO 10
+            [[2,6], rgb_low],   # EDO 11
+            [[3,6], rgb_12 ],   # EDO 12
+            [[4,6], rgb_low],   # EDO 13
+            [[5,6], rgb_low],   # EDO 14
+            [[6,6], rgb_low],   # EDO 15
+            [[7,6], rgb_low],   # EDO 16
+            [[0,5], rgb_low],   # EDO 17
+            [[1,5], rgb_low],   # EDO 18
+            [[2,5], rgb_low],   # EDO 19
+            [[3,5], rgb_low],   # EDO 20
+            [[4,5], rgb_low],   # EDO 21
+            [[5,5], rgb_low],   # EDO 22
+            [[6,5], rgb_low],   # EDO 23
+            [[7,5], rgb_low],   # EDO 24
+            [[0,4], rgb_low],   # EDO 25
+            [[1,4], rgb_low],   # EDO 26
+            [[2,4], rgb_low],   # EDO 27
+            [[3,4], rgb_low],   # EDO 28
+            [[4,4], rgb_low],   # EDO 29
+            [[5,4], rgb_low],   # EDO 30
+            [[6,4], rgb_low],   # EDO 31
+            [[7,4], rgb_low],   # EDO 32
+            [[0,3], rgb_low],   # EDO 33
+            [[1,3], rgb_low],   # EDO 34
+            [[2,3], rgb_low],   # EDO 35
+            [[3,3], rgb_low],   # EDO 36
+            [[4,3], rgb_low],   # EDO 37
+            [[5,3], rgb_low],   # EDO 38
+            [[6,3], rgb_low],   # EDO 39
+            [[7,3], rgb_low],   # EDO 40
+            [[0,2], rgb_low],   # EDO 41
+            [[1,2], rgb_low],   # EDO 42
+            [[2,2], rgb_low],   # EDO 43
+            [[3,2], rgb_low],   # EDO 44
+            [[4,2], rgb_low],   # EDO 45
+            [[5,2], rgb_low],   # EDO 46
+            [[6,2], rgb_low],   # EDO 47
+            [[7,2], rgb_low],   # EDO 48
+            [[0,1], rgb_low],   # EDO 49
+            [[1,1], rgb_low],   # EDO 50
+            [[2,1], rgb_low],   # EDO 51
+            [[3,1], rgb_low],   # EDO 52
+            [[4,1], rgb_low],   # EDO 53
+            [[5,1], rgb_low],   # EDO 54
+            [[6,1], rgb_low],   # EDO 55
+            [[7,1], rgb_low],   # EDO 56
+            [[0,0], rgb_low],   # EDO 57
+            [[1,0], rgb_low],   # EDO 58
+            [[2,0], rgb_low],   # EDO 59
+            [[3,0], rgb_low],   # EDO 60
+            [[4,0], rgb_low],   # EDO 61
+            [[5,0], rgb_low],   # EDO 62
+            [[6,0], rgb_low],   # EDO 63
+            [[7,0], rgb_low],   # EDO 64
+        ])
+        pads.display_text(lpx, 'EDO', False)
+        
